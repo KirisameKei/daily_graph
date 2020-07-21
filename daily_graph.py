13cfe1a9001aafd7e3eb34ff5475373490759729
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2020-07-22 00:11:44 +0900

    いろいろ変更

--- daily_graph.py
+++ daily_graph.py
@@ -5,6 +5,7 @@ import math
 import os
 import time
 import traceback
+import shutil
 
 import bs4
 import openpyxl
@@ -12,6 +13,7 @@ import PIL
 from PIL import Image, ImageDraw, ImageFont
 import requests
 import schedule
+import twitter
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))) #カレントディレクトリを同一階層に(これをやらないとc:\users\username内にファイルを作りやがる)
 
@@ -109,7 +111,7 @@ def draw_graph():
 
         haikei = Image.new(mode="RGB", size=(1550, px), color=0x000000)
 
-        font = ImageFont.truetype(r"c:\Windows\Fonts\UDDigiKyokashoN-R.ttc",size=16) #使用フォントを指定。Windows標準の位置にないとバグる
+        font = ImageFont.truetype(r"./UDDigiKyokashoN-R.ttc",size=16) #使用フォントを指定。Windows標準の位置にないとバグる
 
         hour = ImageDraw.Draw(haikei)
         h_place = 50
@@ -202,24 +204,64 @@ def draw_graph():
         unexpected_error() #こちらでは考えつかないエラーが起きたときunexpected_error()に回す。
 
 
+def twitter_connection():
+    """
+    ツイッターにグラフ画像を投稿"""
+
+    today = datetime.date.today().strftime(r"%Y%m%d")
+
+    with open(f"./{today}.png", mode="rb") as image:
+        image = image.read()
+
+    #consumer_key = os.getenv("consumer_key")
+    #consumer_secret = os.getenv("consumer_secret")
+    #twitter_token = os.getenv("twitter_token")
+    #token_secret = os.getenv("token_secret")
+
+    consumer_key = ""
+    consumer_secret=""
+    twitter_token=""
+    token_secret=""
+    #当初os.getenv()で取っていましたがなぜかNoneを返しやがるので直書きに変更しました
+    #書いてあると思った人、残念だったなwww
+
+    auth = twitter.OAuth(consumer_key=consumer_key,
+                        consumer_secret=consumer_secret,
+                        token=twitter_token,
+                        token_secret=token_secret)
+
+    t = twitter.Twitter(auth=auth)
+
+    pic_upload = twitter.Twitter(domain="upload.twitter.com", auth=auth)
+    id_img1 = pic_upload.media.upload(media=image)["media_id_string"]
+    t.statuses.update(status=f"#整地鯖\n{today}", media_ids=",".join([id_img1]))
+
 def push_files():
     """
     日間上位20人の時間毎の整地量をまとめたJSONファイルとxlsxファイルをウェブフックを使い投稿する"""
 
     try:
         today = datetime.date.today().strftime(r"%Y%m%d") #str型の今日の日付
-        with open(f"{today}.json", mode="r") as f1: #本日の上位20人のデータをまとめたJSONファイルを指定
-            with open(f"{today}.xlsx",mode="rb") as f2: #本日の上位20人のデータをまとめたxlsxファイルを指定
-                with open(f"{today}.png", mode="rb") as f3: #本日の上位20人のグラフのpngファイルを指定
-                    files = { #指定したファイル達
-                        "file1": f1,
-                        "file2": f2,
-                        "file3": f3
-                    }
-                    break_webhook_url = "https://discordapp.com/api/webhooks/以下略"
-                    requests.post(break_webhook_url, files=files) #JSONファイルをウェブフックに投稿
+        source = "./player_data.json"
+        copy = f"./player_data{today}.json"
+        shutil.copy(source, copy)
+        with open(f"player_data{today}.json", mode="r") as f0: #本日の
+            with open(f"{today}.json", mode="r") as f1: #本日の上位20人のデータをまとめたJSONファイルを指定
+                with open(f"{today}.xlsx",mode="rb") as f2: #本日の上位20人のデータをまとめたxlsxファイルを指定
+                    with open(f"{today}.png", mode="rb") as f3: #本日の上位20人のグラフのpngファイルを指定
+                        files = { #指定したファイル達
+                            "file0": f0,
+                            "file1": f1,
+                            "file2": f2,
+                            "file3": f3
+                        }
+                        break_webhook_url = "https://discordapp.com/api/webhooks/以下略"
+                        requests.post(break_webhook_url, files=files) #JSONファイルをウェブフックに投稿
 
-        os.remove(f"{today}.json") #ファイルが増え続けていくので送信したら消す
+        twitter_connection()
+
+        os.remove(f"player_data{today}.json") #ファイルが増え続けていくので送信したら消す
+        os.remove(f"{today}.json") #同上
         os.remove(f"{today}.xlsx") #同上
         os.remove(f"{today}.png") #同上
 
@@ -236,7 +278,10 @@ def login():
             "content": "プログラムが起動しました"
         }
     requests.post(login_webhook_url, main_content) #ログインメッセをウェブフックに投稿
-    print("起動しました")
+    try:
+        print("起動しました")
+    except UnicodeEncodeError:
+        pass
 
 
 def job_every_hour():
@@ -253,53 +298,101 @@ def job_every_hour():
         except FileNotFoundError:
             player_data_dict = {}
 
-        #KeyErrorが出れば異常なし。出なかった場合以前のデータ取得に失敗している
-        try:
-            if player_data_dict["today"] == "Error":
-                pass
-        except KeyError:
-        
-            #整地鯖ランキングページにアクセス&データを記録する
-            i = 0
-            while True:
-                url = f"https://w4.minecraftserver.jp/api/ranking?type=break&offset={i*20}&lim=20&duration=daily"
-                try:
-                    res = requests.get(url) #整地鯖ランキングページにアクセス
-                    res.raise_for_status() #これは自分でもよくわからん。書いておかないとおかしくなる
-                    player_data_every_hour_dict = ast.literal_eval(str(bs4.BeautifulSoup(res.text, "html.parser"))) #全体を辞書型で取得
-                    player_data_every_hour_list = player_data_every_hour_dict["ranks"] #必要な部分のみリスト型で取得
+        #整地鯖ランキングページにアクセス&データを記録する
+        i = 0
+        while True:
+            url = f"https://w4.minecraftserver.jp/api/ranking?type=break&offset={i*20}&lim=20&duration=daily"
+            try:
+                res = requests.get(url) #整地鯖ランキングページにアクセス
+                res.raise_for_status() #これは自分でもよくわからん。書いておかないとおかしくなる
+                soup = bs4.BeautifulSoup(res.text, "html.parser") #特に意味のないコメント(けい制作srcコピペ防止)
+                player_data_every_hour_dict = json.loads(soup.decode("utf-8")) #全体を辞書型で取得
+                player_data_every_hour_list = player_data_every_hour_dict["ranks"] #必要な部分のみリスト型で取得
 
-                    for player_data_every_hour in player_data_every_hour_list: #リストから一つずつデータを抜き出す
-                        mcid = player_data_every_hour["player"]["name"] #リストからMCIDを取得
-                        raw_data = player_data_every_hour["data"]["raw_data"] #リストから整地量を取得
-                        now = datetime.datetime.now() #今何時？
-                        #以前からplayer_dataがあれば追記、なければ新規作成
-                        try:
-                            player_data = player_data_dict[mcid]
-                        except KeyError:
-                            player_data = {}
-                        player_data[f"{now.hour}"] = int(raw_data) #プレイヤーの時間毎の整地量が格納される辞書
-                        player_data_dict[mcid] = player_data #上記の辞書を全MCIDぶん格納する辞書
+                for player_data_every_hour in player_data_every_hour_list: #リストから一つずつデータを抜き出す
+                    mcid = player_data_every_hour["player"]["name"] #リストからMCIDを取得
+                    raw_data = player_data_every_hour["data"]["raw_data"] #リストから整地量を取得
+                    now = datetime.datetime.now() #今何時？
+                    #以前からplayer_dataがあれば追記、なければ新規作成
+                    try:
+                        player_data = player_data_dict[mcid]
+                    except KeyError:
+                        player_data = {}
+                    player_data[f"{now.hour}"] = int(raw_data) #プレイヤーの時間毎の整地量が格納される辞書
+                    player_data_dict[mcid] = player_data #上記の辞書を全MCIDぶん格納する辞書
 
-                    #まだ続きがありそうなら続ける、なければループを抜ける
-                    if len(player_data_every_hour_list) == 20:
-                        i += 1
-                    else:
-                        break
-                #ウェブページに不調があったときの処理
-                except requests.exceptions.HTTPError:
-                    main_content = {
-                        "content": "今日は無理だな(確信)"
-                    }
-                    break_webhook_url = "https://discordapp.com/api/webhooks/以下略"
-                    requests.post(break_webhook_url, main_content) #諦めメッセをウェブフックに投稿
-                    player_data_dict = {"today":"Error"} #辞書の中身をエラー発生時用に変更、こうなってると70行目で省かれてexcept内は実行されない
+                #まだ続きがありそうなら続ける、なければループを抜ける
+                if len(player_data_every_hour_list) == 20:
+                    i += 1
+                else:
                     break
+            #ウェブページに不調があったときの処理
+            except requests.exceptions.HTTPError:
+                now_hour = datetime.datetime.now().hour #今何時？
+                one_hour_ago = now_hour - 1 #1時間前は何時？
+                for player_data in player_data_dict.values(): #全データを一つずつ取り出す
+                    player_data[f"{now_hour}"] = player_data[f"{one_hour_ago}"] #1時間前と同じ値を記録
+                break
 
-            #作成したplayer_data_dictをjson形式に変換して保存
-            with open("player_data.json", mode="w") as f: #書き込みモードで開く
-                player_data_json = json.dumps(player_data_dict, indent=4) #JSON形式に変換
-                f.write(player_data_json) #書く
+        #作成したplayer_data_dictをjson形式に変換して保存
+        with open("player_data.json", mode="w") as f: #書き込みモードで開く
+            player_data_json = json.dumps(player_data_dict, indent=4) #JSON形式に変換
+            f.write(player_data_json) #書く
+
+    except:
+        unexpected_error() #こちらでは考えつかないエラーが起きたときunexpected_error()に回す。
+
+
+def job_23_58():
+    """
+    23:58分に実行する処理
+    player_data.jsonを完成させる"""
+
+    try:
+        #player_data.jsonが見つからなかった時の対処
+        try:
+            with open("player_data.json", mode="r") as f:
+                player_data_dict = json.load(f)
+        except FileNotFoundError:
+            player_data_dict = {}
+
+        #整地鯖ランキングページにアクセス&データを記録する
+        i = 0
+        while True:
+            url = f"https://w4.minecraftserver.jp/api/ranking?type=break&offset={i*20}&lim=20&duration=daily"
+            try:
+                res = requests.get(url) #整地鯖ランキングページにアクセス
+                res.raise_for_status() #これは自分でもよくわからん。書いておかないとおかしくなる
+                soup = bs4.BeautifulSoup(res.text, "html.parser")
+                player_data_every_hour_dict = json.loads(soup.decode("utf-8")) #全体を辞書型で取得
+                player_data_every_hour_list = player_data_every_hour_dict["ranks"] #必要な部分のみリスト型で取得
+
+                for player_data_every_hour in player_data_every_hour_list: #リストから一つずつデータを抜き出す
+                    mcid = player_data_every_hour["player"]["name"] #リストからMCIDを取得
+                    raw_data = player_data_every_hour["data"]["raw_data"] #リストから整地量を取得
+                    #以前からplayer_dataがあれば追記、なければ新規作成
+                    try:
+                        player_data = player_data_dict[mcid]
+                    except KeyError:
+                        player_data = {}
+                    player_data["23_58"] = int(raw_data) #プレイヤーの時間毎の整地量が格納される辞書
+                    player_data_dict[mcid] = player_data #上記の辞書を全MCIDぶん格納する辞書
+
+                #まだ続きがありそうなら続ける、なければループを抜ける
+                if len(player_data_every_hour_list) == 20:
+                    i += 1
+                else:
+                    break
+            #ウェブページに不調があったときの処理
+            except requests.exceptions.HTTPError:
+                for player_data in player_data_dict.values(): #全データを一つずつ取り出す
+                    player_data["23_58"] = player_data["23"] #1時間前と同じ値を記録
+                break
+
+        #作成したplayer_data_dictをjson形式に変換して保存
+        with open("player_data.json", mode="w") as f: #書き込みモードで開く
+            player_data_json = json.dumps(player_data_dict, indent=4) #JSON形式に変換
+            f.write(player_data_json) #書く
 
     except:
         unexpected_error() #こちらでは考えつかないエラーが起きたときunexpected_error()に回す。
@@ -308,9 +401,12 @@ def job_every_hour():
 def job_every_day():
     """
     毎日の処理
-    毎日23時58分に整地鯖ランキングページにアクセスし、上位20人のMCID・整地量・順位を取得。"""
+    毎日23時58分に整地鯖ランキングページにアクセスし、
+    job_23_58を実行したのち上位20人のMCID・整地量・順位を取得。"""
 
     try:
+        job_23_58()
+
         break_webhook_url = "https://discordapp.com/api/webhooks/以下略"
         now = datetime.datetime.now() #datetime型の現在時刻
         #そもそもファイルなしを省く
@@ -324,67 +420,54 @@ def job_every_day():
             requests.post(break_webhook_url, main_content) #結果をウェブフックに投稿
             return
 
-        #ファイルはあったけどどこかでエラーが起きた時を省く
+        daily_player_data_dict = {}
+        url = f"https://w4.minecraftserver.jp/api/ranking?type=break&offset=0&lim=20&duration=daily" #今度は全MCIDではなく上位20人のみ
         try:
-            if player_data_dict["today"] == "Error":
-                main_content = {
-                    "content": f"{now.month}月{now.day}日 : データなし"
-                }
-                requests.post(break_webhook_url, main_content) #結果をウェブフックに投稿
-                player_data_dict.clear() #翌日のためにplayer_data_dictを空にする
-                with open("player_data.json", mode="w") as f:
-                    player_data_json = json.dumps(player_data_dict, indent=4)
-                    f.write(player_data_json)
-                return
-
-        #KeyErrorが出れば正常。処理を行う
-        except KeyError:
-            daily_player_data_dict = {}
-            url = f"https://w4.minecraftserver.jp/api/ranking?type=break&offset=0&lim=20&duration=daily" #今度は全MCIDではなく上位20人のみ
-            try:
-                res = requests.get(url) #さっきと同じ
-                res.raise_for_status()
-                player_data_every_day_dict = ast.literal_eval(str(bs4.BeautifulSoup(res.text, "html.parser")))
-                player_data_every_day_list = player_data_every_day_dict["ranks"]
-                for player_data_every_day in player_data_every_day_list:
-                    mcid = player_data_every_day["player"]["name"]
+            res = requests.get(url) #さっきと同じ
+            res.raise_for_status()
+            soup = bs4.BeautifulSoup(res.text, "html.parser")
+            player_data_every_day_dict = json.loads(soup.decode("utf-8")) #全体を辞書型で取得
+            player_data_every_day_list = player_data_every_day_dict["ranks"]
+            for player_data_every_day in player_data_every_day_list:
+                mcid = player_data_every_day["player"]["name"]
+                try:
+                    player_data = player_data_dict[mcid]
+                except KeyError:
+                    player_data = {}
+                hour_break = {}
+                for hour in range(24): #0～23の24回ループ
                     try:
-                        player_data = player_data_dict[mcid]
+                        break_amount = player_data[f"{hour}"] #時間毎の整地量を取得
                     except KeyError:
-                        player_data = {}
-                    hour_break = {}
-                    for hour in range(24): #0～23の24回ループ
-                        try:
-                            break_amount = player_data[f"{hour}"] #時間毎の整地量を取得
-                        except KeyError:
-                            break_amount = 0 #整地されていなければ0を入力
-                        hour_break[f"{hour}"] = break_amount
-                    raw_data = player_data_every_day["data"]["raw_data"] #23:58分の整地量を取得
-                    hour_break["23_58"] = int(raw_data) #整地量を記録
-                    daily_player_data_dict[mcid] = hour_break #MCIDごとに時間毎の整地量を記録
-                today = datetime.date.today().strftime(r"%Y%m%d") #str型の今日の日付
-                with open(f"{today}.json", mode="w") as f:
-                    daily_player_data_json = json.dumps(daily_player_data_dict, indent=4)
-                    f.write(daily_player_data_json) #記録したデータをJSON形式で書き込み
-                
-                player_data_dict.clear() #翌日のためにplayer_data_dictを空にする
-                with open("player_data.json", mode="w") as f:
-                    player_data_json = json.dumps(player_data_dict, indent=4)
-                    f.write(player_data_json)
-                pyxl() #データをエクセルに入力する関数
-                draw_graph() #グラフを作成する関数
-                push_files() #ファイルをウェブフックに投稿する関数
+                        break_amount = 0 #整地されていなければ0を入力
+                    hour_break[f"{hour}"] = break_amount
+                raw_data = player_data_every_day["data"]["raw_data"] #23:58分の整地量を取得
+                hour_break["23_58"] = int(raw_data) #整地量を記録
+                daily_player_data_dict[mcid] = hour_break #MCIDごとに時間毎の整地量を記録
+            today = datetime.date.today().strftime(r"%Y%m%d") #str型の今日の日付
+            with open(f"{today}.json", mode="w") as f:
+                daily_player_data_json = json.dumps(daily_player_data_dict, indent=4)
+                f.write(daily_player_data_json) #記録したデータをJSON形式で書き込み
 
-            #ウェブページに不調があったときの処理
-            except requests.exceptions.HTTPError:
-                main_content = {
-                    "content": f"{now.month}月{now.day}日 : データなし"
-                }
-                requests.post(break_webhook_url, main_content) #結果をウェブフックに投稿
-                player_data_dict.clear() #翌日のためにplayer_data_dictを空にする
-                with open("player_data.json", mode="w") as f:
-                    player_data_json = json.dumps(player_data_dict, indent=4)
-                return
+            pyxl() #データをエクセルに入力する関数
+            draw_graph() #グラフを作成する関数
+            push_files() #ファイルをウェブフックに投稿する関数
+
+            player_data_dict.clear() #翌日のためにplayer_data_dictを空にする
+            with open("player_data.json", mode="w") as f:
+                player_data_json = json.dumps(player_data_dict, indent=4)
+                f.write(player_data_json)
+
+        #ウェブページに不調があったときの処理
+        except requests.exceptions.HTTPError:
+            main_content = {
+                "content": f"{now.month}月{now.day}日 : データなし"
+            }
+            requests.post(break_webhook_url, main_content) #結果をウェブフックに投稿
+            player_data_dict.clear() #翌日のためにplayer_data_dictを空にする
+            with open("player_data.json", mode="w") as f:
+                player_data_json = json.dumps(player_data_dict, indent=4)
+            return
     except:
         unexpected_error() #こちらでは考えつかないエラーが起きたときunexpected_error()に回す。
 
